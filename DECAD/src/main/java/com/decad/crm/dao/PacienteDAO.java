0de5f39a3e0cee15c368c2105e0eb38f4eb5603a
Eduardo Gonçalves <eduardouc388@gmail.com>
2025-10-20 22:15:14 -0300

    Método salvarPaciente implementado

--- DECAD/src/main/java/com/decad/crm/dao/PacienteDAO.java
+++ DECAD/src/main/java/com/decad/crm/dao/PacienteDAO.java
@@ -5,6 +5,7 @@ import com.decad.crm.util.ConectorBancoDeDados;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PacienteDAO {
@@ -20,13 +21,33 @@ public class PacienteDAO {
             statement.setString(3, paciente.getCpf());
             statement.setString(4, paciente.getTelefone());
 
-            statement.executeQuery();
+            int linhasAfetadas = statement.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                System.out.println("Paciente salvo com sucesso!");
+            }
 
         } catch (SQLException e) {
             System.out.println("Erro ao salvar Paciente: " + e.getMessage());
             throw new RuntimeException("Erro ao salvar Paciente: " + e);
         }
+    }
 
+    public void buscarPacientePorId(long id) {
+        String sql = "SELECT * FROM Paciente WHERE id = ?";
+
+        try(Connection conexao = ConectorBancoDeDados.conectar();
+            PreparedStatement statement = conexao.prepareStatement(sql)) {
+
+            statement.setLong(1, id);
+            ResultSet rs = statement.executeQuery();
+
+            //em desenvolvimento...
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao buscar Paciente: " + e.getMessage());
+            throw new RuntimeException("Erro ao buscar Paciente: " + e);
+        }
     }
 
 
